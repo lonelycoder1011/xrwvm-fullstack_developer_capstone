fc96a96c1a09bcaaab819ca6c9284a9323c06dd6
Muhammad Ali Hasnain <muhammadalihasnain23@gmail.com>
2025-01-26 11:02:17 -0800

    Revert "Build Car Models"
    This reverts commit 78aadfeab7a3039a57618a29a8075856557b315e, reversing
    changes made to 0dc67ed7e2d5b6ff8e5f6942f967b6e161d796da.

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('login', views.login_user, name='login'),
     path('logout', views.logout_request, name='logout'),
     path('register', views.registration, name='register'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view (unimplemented)
 
